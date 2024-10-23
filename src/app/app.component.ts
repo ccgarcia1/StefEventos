cd7a4bcb47547ed198eae6854abd2d1071ce13bd
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-10-23 14:13:00 -0300

    Adding new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent} from './componente/cabecalho/cabecalho.component'
+import { RodapeComponent } from './componente/rodape/rodape.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CabecalhoComponent],
+  imports: [RouterOutlet,CabecalhoComponent,RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { SobreComponent } from './pagina/sobre/sobre.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { EventosComponent } from './pagina/eventos/eventos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'inicio', component: InicioComponent},
+    {path:'sobre',component:SobreComponent},
+    {path:'contato',component:ContatoComponent},
+    {path:'eventos' ,component:EventosComponent}
+];
