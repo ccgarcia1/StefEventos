b77440ff8e10faa7ba6090b58c2d33ce4af34b13
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-10-29 20:53:54 -0300

    Adding form validation

--- src/app/pagina/contato/contato.component.ts
+++ src/app/pagina/contato/contato.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule,CommonModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
+  formContato: FormGroup;
+
+  contatoEnviado : boolean = false;
+
+  constructor(private formBuilder: FormBuilder){
+    this.formContato = this.formBuilder.group({
+      nome : ["",Validators.required],
+      email : ["",Validators.required],
+      mensagem : ["",[Validators.required,Validators.minLength(5)]],
+    });
+  }
+  onsend(){
+    if (this.formContato.valid){
+      console.log("Nome: ", this.formContato.get('nome')?.value);
+      console.log("email: ", this.formContato.get('email')?.value);
+      console.log("mensagem: ", this.formContato.get('mensagem')?.value,)
+      
+      this.contatoEnviado = true;
+    }
+  }
 
 }
